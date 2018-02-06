444ebed5e681bf4678785b764c3feb38c3cba2b8
'zxd' <zxd@zxd.com>
2018-02-06 18:53:05 +0800

    c

--- redis/src/main/java/com/rb/sz/cmb/controller/CityRestController.java
+++ redis/src/main/java/com/rb/sz/cmb/controller/CityRestController.java
@@ -3,13 +3,19 @@ package com.rb.sz.cmb.controller;
 
 import java.util.List;
 
+import org.bouncycastle.jcajce.provider.asymmetric.ec.SignatureSpi.ecCVCDSA;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rb.sz.cmb.domain.City;
+import com.rb.sz.cmb.domain.LoginInfo;
 import com.rb.sz.cmb.service.CityService;
+import com.rb.sz.cmb.service.LoginInfoService;
 import com.rb.sz.cmb.service.impl.RedisService;
 
 /**
@@ -22,6 +28,8 @@ public class CityRestController {
     private CityService cityService;
     @Autowired
     private RedisService redisService;
+    @Autowired
+    private LoginInfoService loginInfoService;
     
     @RequestMapping("/list")
     public String list() {
@@ -29,7 +37,24 @@ public class CityRestController {
     	 System.out.println(redisService.getValue("dan"));
     	 List<City> list= cityService.list();
          System.out.println(list.toString());
-         return "city";
+         return null;
     }
-
+    
+    @RequestMapping("/log")
+    public String log() {
+    	 List<LoginInfo> list= loginInfoService.list();
+         System.out.println(list.toString());
+         return null;
+    }
+    
+    @RequestMapping("/add")
+    @Transactional(propagation = Propagation.REQUIRED,isolation = Isolation.DEFAULT,timeout=36000,rollbackFor=Exception.class)
+    public String add() {
+    	 try {
+    		 loginInfoService.insert();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	 return null;
+    }
 }
